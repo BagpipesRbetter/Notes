b46b66ad9d21b6e821804a2631f8cb46d8c49f73
BagpipesRbetter <119373672+BagpipesRbetter@users.noreply.github.com>
2024-10-17 12:03:18 -0600

    Pretty the code :)

--- Docs/js/tree.js
+++ Docs/js/tree.js
@@ -1,31 +1,38 @@
 const buildTreeData = async (url) => {
-    const response = await fetch(url);
-    const files = await response.json();
-    const treeData = [];
+  const response = await fetch(url);
+  const files = await response.json();
+  const treeData = [];
 
-    for (let file of files) {
-        if (file.type === 'file' && file.name.endsWith('.md')) {
-            treeData.push({ title: file.name, path: file.path, icon: "file" });
-        } else if (file.type === 'file' && /\.(jpg|jpeg|png|gif|svg)$/i.test(file.name)) {
-            treeData.push({ title: file.name, path: file.path, icon: "image" });
-        } else if (file.type === 'dir') {
-            treeData.push({ title: file.name, folder: true, children: await buildTreeData(file.url) });
-        }
+  for (let file of files) {
+    if (file.type === "file" && file.name.endsWith(".md")) {
+      treeData.push({ title: file.name, path: file.path, icon: "file" });
+    } else if (
+      file.type === "file" &&
+      /\.(jpg|jpeg|png|gif|svg)$/i.test(file.name)
+    ) {
+      treeData.push({ title: file.name, path: file.path, icon: "image" });
+    } else if (file.type === "dir") {
+      treeData.push({
+        title: file.name,
+        folder: true,
+        children: await buildTreeData(file.url),
+      });
     }
+  }
 
-    return treeData;
+  return treeData;
 };
 
 const renderTree = async () => {
-    const treeData = await buildTreeData(apiUrl);
+  const treeData = await buildTreeData(apiUrl);
 
-    // Initialize FancyTree
-    $("#file-tree").fancytree({
-        source: treeData,
-        click: async function(event, data) {
-            if (!data.node.folder) {
-                await fetchAndRenderMarkdown(data.node.data.path);
-            }
-        }
-    });
+  // Initialize FancyTree
+  $("#file-tree").fancytree({
+    source: treeData,
+    click: async function (event, data) {
+      if (!data.node.folder) {
+        await fetchAndRenderMarkdown(data.node.data.path);
+      }
+    },
+  });
 };
